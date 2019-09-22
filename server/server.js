9a63de19d041c25b7f66aca05fd72f3073f23798
Michael Roberts <michael@origincodeacademy.com>
2019-09-22 13:24:52 -0700

    adds route to proxy requests

--- server/server.js
+++ server/server.js
@@ -1,10 +1,20 @@
+require('dotenv').config();
+
 const express = require('express');
 const morgan = require('morgan');
+const axios = require('axios');
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.static('dist'));
 app.use(express.static('public'));
+
+app.get('/search/:city', (req, res) => {
+    cityName = req.params.city;
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.OPEN_WEATHER_API_KEY}&units=imperial`)
+            .then(response => res.send(response.data))
+            .catch(err => console.log(err));
+});
 
 module.exports = app;
